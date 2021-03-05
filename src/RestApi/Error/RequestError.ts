51557748a4ba01b6cc5205e4f1bc4904dc2126a2
Michael Zabka <zabka.michael@gmail.com>
2021-03-05 21:13:09 +0000

    Add more detailed response error with possible errorCode & errorName

--- src/RestApi/Error/RequestError.ts
+++ src/RestApi/Error/RequestError.ts
@@ -1,9 +1,16 @@
 
 export default class RequestError extends Error {
 
+	public readonly errorCode: number | undefined;
+	public readonly errorName: string | undefined;
+
 	constructor(status: number, body: any) {
 		super(`Request failed with status code ${status}. Body: ${JSON.stringify(body)}`);
 		// https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
 		Object.setPrototypeOf(this, RequestError.prototype);
+		if (typeof body === 'object') {
+			this.errorCode = body.errorCode;
+			this.errorName = body.errorName;
+		}
 	}
 }
