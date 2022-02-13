97fe4c906f25cc060da43b5b7c263a35c1a71497
PetrDusek <petr.dusek@signageos.io>
2022-02-13 15:16:31 +0100

    add bulk operation api sdk tests

---
+++ tests/Unit/RestApi/BulkOperation/BulkOperationManagement.spec.ts
@@ -0,0 +1,125 @@
+import * as should from 'should';
+import * as nock from 'nock';
+import { nockOpts, nockAuthHeader, successRes } from '../helper';
+import { IBulkOperationCreatable } from '../../../../src/RestApi/BulkOperation/IBulkOperation';
+import { DeviceActionType } from '../../../../src/RestApi/BulkOperation/enums';
+import BulkOperation from '../../../../src/RestApi/BulkOperation/BulkOperation';
+import BulkOperationManagement from '../../../../src/RestApi/BulkOperation/BulkOperationManagement';
+
+const bulkOperationResource = 'bulk-operation';
+
+describe('BulkOperationManagement', () => {
+	const getLocationHeader = (bulkOperationUid: string): nock.HttpHeaders => ({
+		Location: `${nockOpts.url}/${nockOpts.version}/${bulkOperationResource}/${bulkOperationUid}`,
+	});
+
+	const bulkOperationDb = new BulkOperation({
+		uid: 'testingUid',
+		deviceUids: [],
+		successfulDeviceUids: [],
+		failedDeviceUids: [],
+		name: 'testingName4',
+		filter: {
+			applicationType: 'tizen',
+		},
+		createdAt: 'createdAtDate',
+		scheduleDate: 'testingDate',
+		rollingUpdate: {
+			batchSize: 10,
+			batchDelay: 1000,
+			stopThreshold: 50,
+		},
+		operationType: DeviceActionType.SET_APPLICATION_VERSION,
+		data: {
+			applicationType: 'tizen',
+			version: 'testingVersion',
+		},
+		progress: {
+			total: 0,
+			succeeded: 0,
+			failed: 0,
+			inProgress: 0,
+		},
+		organizationUid: 'testingOrganizationUid',
+	});
+
+	const rollingUpdateRequest = {
+		rollingUpdate: {
+			batchSize: 10,
+			batchDelay: 5000,
+			stopThreshold: 30,
+		},
+	};
+
+	const bulkOperationRequestCreate: IBulkOperationCreatable<DeviceActionType> = {
+		name: 'testingName4',
+		filter: {
+			applicationType: 'tizen',
+		},
+		scheduleDate: {
+			timestamp: Date.now(),
+			timeZoneOffset: new Date().getTimezoneOffset(),
+		},
+		rollingUpdate: {
+			batchSize: 10,
+			batchDelay: 1000,
+			stopThreshold: 50,
+		},
+		operationType: DeviceActionType.SET_APPLICATION_VERSION,
+		data: {
+			applicationType: 'tizen',
+			version: 'testingVersion',
+		},
+	};
+
+	nock(nockOpts.url, nockAuthHeader)
+		.persist()
+		.get(`/v1/${bulkOperationResource}`)
+		.query({
+			limit: 1,
+			offset: 1,
+		})
+		.reply(200, [bulkOperationDb])
+		.get(`/v1/${bulkOperationResource}/bulkOperationUid`)
+		.reply(200, bulkOperationDb)
+		.post(`/v1/${bulkOperationResource}`, bulkOperationRequestCreate)
+		.reply(201, successRes, getLocationHeader(`bulkOperationUid`))
+		.put(`/v1/${bulkOperationResource}/stop/bulkOperationUid`, {})
+		.reply(200, successRes)
+		.put(`/v1/${bulkOperationResource}/pause/bulkOperationUid`, {})
+		.reply(200, successRes)
+		.put(`/v1/${bulkOperationResource}/resume/bulkOperationUid`, rollingUpdateRequest)
+		.reply(200, successRes);
+
+	const bulkOperationManagement = new BulkOperationManagement(nockOpts);
+
+	it('should get bulk operation by its uid', async () => {
+		const bulkOperation = await bulkOperationManagement.get('bulkOperationUid');
+		should(bulkOperation).deepEqual(bulkOperationDb);
+	});
+
+	it('should create bulk operation', async () => {
+		const bulkOperation = await bulkOperationManagement.create(bulkOperationRequestCreate);
+		should(bulkOperation).deepEqual(bulkOperationDb);
+	});
+
+	it('should get bulk operations by organizationUid', async () => {
+		const bulkOperation = await bulkOperationManagement.list({
+			limit: 1,
+			offset: 1,
+		});
+		should(bulkOperation[0]).deepEqual(bulkOperationDb);
+	});
+
+	it('should stop bulk operation by uid', async () => {
+		await should(bulkOperationManagement.stop('bulkOperationUid')).be.fulfilled();
+	});
+
+	it('should pause bulk operation by uid', async () => {
+		await should(bulkOperationManagement.pause('bulkOperationUid')).be.fulfilled();
+	});
+
+	it('should resume bulk operation by uid', async () => {
+		await should(bulkOperationManagement.resume('bulkOperationUid', rollingUpdateRequest)).be.fulfilled();
+	});
+});
