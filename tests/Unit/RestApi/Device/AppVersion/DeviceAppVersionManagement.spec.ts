3bda52f0e673488e2164acd94e46e08be95059bd
Josef Král <josef.kral@signageos.io>
2022-04-27 11:40:38 +0200

    FIx incorrect URL path on updating device application version

--- tests/Unit/RestApi/Device/AppVersion/DeviceAppVersionManagement.spec.ts
+++ tests/Unit/RestApi/Device/AppVersion/DeviceAppVersionManagement.spec.ts
@@ -6,18 +6,20 @@ import DeviceAppVersionManagement from "../../../../../src/RestApi/Device/AppVer
 
 describe('DeviceAppVersionManagement', () => {
 
+	const applicationType: string = "tizen";
+
 	const validGetResp: IDeviceAppVersion = {
 		uid: 'someUid',
 		deviceUid: '3caXXX589b',
 		version: '1.0.0',
-		applicationType: 'tizen',
+		applicationType: applicationType,
 		createdAt: new Date('2018-05-23T14:37:07.362Z'),
 		succeededAt: null,
 		failedAt: null,
 	};
 	const validSetReq: IDeviceAppVersionUpdatable = {
 		version: '1.0.1',
-		applicationType: 'tizen',
+		applicationType: applicationType,
 	};
 
 	const davm = new DeviceAppVersionManagement(nockOpts);
@@ -29,7 +31,7 @@ describe('DeviceAppVersionManagement', () => {
 			},
 		})
 		.get('/v1/device/someUid/application/version').reply(200, validGetResp)
-		.put('/v1/device/someUid/application/version', validSetReq).reply(200, successRes);
+		.put(`/v1/device/someUid/application/${applicationType}/version`, validSetReq).reply(200, successRes);
 
 	describe('get application version', () => {
 		it('should parse the response', async () => {
