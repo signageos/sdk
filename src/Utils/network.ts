483893e03a99f5cdb8cd7f81fe6c2c8632cf4625
Michael Zabka <zabka.michael@gmail.com>
2023-03-10 15:40:35 +0000

    Add network util with getMachineRemoteAddr function

---
+++ src/Utils/network.ts
@@ -0,0 +1,21 @@
+import { networkInterfaces } from "os";
+
+/**
+ * Returns first IPv4 address of the machine.
+ * It's useful for local development server.
+ */
+export function getMachineRemoteAddr(): string {
+	const nets = networkInterfaces(), machineIps = Object.create(null);
+	for (const name of Object.keys(nets)) {
+		for (const net of nets[name] ?? []) {
+			if (net.family === 'IPv4' && !net.internal) {
+				if (!machineIps[name]) {
+					machineIps[name] = [];
+				}
+				machineIps[name].push(net.address);
+			}
+		}
+	}
+	const firsInterface = Object.keys(machineIps)[0];
+	return  machineIps[firsInterface][0];
+}
