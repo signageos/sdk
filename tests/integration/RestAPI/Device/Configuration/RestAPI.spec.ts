dba34aa3eed30ff0760a7d1eac0df55b9aab2006
alisher <alishern@seznam.cz>
2023-04-25 13:21:56 +0200

    add test validation of telemetry values

--- tests/integration/RestAPI/Device/Configuration/RestAPI.spec.ts
+++ tests/integration/RestAPI/Device/Configuration/RestAPI.spec.ts
@@ -26,6 +26,7 @@ describe('RestAPI - Device Configuration', () => {
 			try {
 				// @ts-ignore ignore not assignable parameter that is the point of a test
 				await api.device.configuration.setTelemetryIntervals(device.uid, { 'battery+': 1500000 });
+				should(true).be.equal('this should not be reached');
 			} catch (error) {
 				should(error.message).be.equal(
 					`Request failed with status code 400. Body: ${JSON.stringify({
@@ -34,6 +35,24 @@ describe('RestAPI - Device Configuration', () => {
 						errorCode: 400301,
 						errorName: 'INVALID_BODY_PROPERTIES',
 						errorDetail: ' Received unrecognized keys battery+.',
+					})}`,
+				);
+			}
+		});
+
+		it('should fail due to invalid value provided', async () => {
+			try {
+				const ONE_YEAR_IN_MILLISECONDS_PLUS_ONE = 31556926 * 1e3 + 1;
+				await api.device.configuration.setTelemetryIntervals(device.uid, { battery: ONE_YEAR_IN_MILLISECONDS_PLUS_ONE });
+				should(true).be.equal('this should not be reached');
+			} catch (error) {
+				should(error.message).be.equal(
+					`Request failed with status code 400. Body: ${JSON.stringify({
+						status: 400,
+						message: 'Invalid request body - battery - Value should be a positive number and less than or equal to 31556926000 (1 year in milliseconds)',
+						errorCode: 400301,
+						errorName: 'INVALID_BODY_PROPERTIES',
+						errorDetail: 'Received invalid body properties battery.',
 					})}`,
 				);
 			}
