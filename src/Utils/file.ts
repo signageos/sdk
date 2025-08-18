693dafc6af7159caed958be44efa02546d034482
Mikuláš Sychra <mikulas.sychra@gmail.com>
2025-08-18 18:13:56 +0200

    Test fixes

--- src/Utils/file.ts
+++ src/Utils/file.ts
@@ -1,7 +1,7 @@
-import { fileTypeFromBuffer } from 'file-type/node';
-
 // TODO: Copied from @signageos/lib, we can add this lib to dependencies ?
 export const getFileExtension = async (file: Buffer) => {
+	// Dynamic import to handle ESM-only file-type package in CommonJS environment
+	const { fileTypeFromBuffer } = await import('file-type');
 	const fileType = await fileTypeFromBuffer(file);
 	const extension = fileType?.ext ?? fileType?.mime;
 
